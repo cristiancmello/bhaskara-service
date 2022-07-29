92564e5a15e5af4395508d54298772fab16f4d37
Cristian Mello <cristianc.mello@gmail.com>
2022-07-29 18:59:34 -0300

    Implementando teste do exemplo 1 given_ParametrosTexto_whenParametrosConvertidosReal_thenCalculeRaizes

---
+++ src/main/java/com/bhaskara/service/Bhaskara.java
@@ -0,0 +1,19 @@
+package com.bhaskara.service;
+
+import java.util.ArrayList;
+
+public class Bhaskara {
+
+    public static ArrayList<Double> calcularRaizes(double a, double b, double c) {
+        var conjuntoRaizes = new ArrayList<Double>();
+
+        var delta = Math.pow(b, 2) + -4 * a * c; // b^2 -4ac
+        var x1 = (-b + Math.sqrt(delta)) / 2 * a;
+        var x2 = (-b - Math.sqrt(delta)) / 2 * a;
+
+        conjuntoRaizes.add(x1);
+        conjuntoRaizes.add(x2);
+
+        return conjuntoRaizes;
+    }
+}
---
+++ src/test/java/com/bhaskara/service/BhaskaraTests.java
@@ -0,0 +1,25 @@
+package com.bhaskara.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+// TDD
+// [1] Criar um teste que falhe (RED)
+//     Então precisamos fazer com que o teste passe para sair desta etapa.
+//     O código precisa ser o mais simples possivel. Podemos cometer pecados.
+// [2] Fazer que o código que vai passar (GREEN)
+// [3] Vamos refatorar o código para refletir a implementação real (REFACT)
+class BhaskaraTests {
+	@Test
+	void given_ParametrosTexto_whenParametrosConvertidosReal_thenCalculeRaizes() {
+		String inputA = "1", inputB = "-2", inputC = "-3";
+
+		double a = Double.parseDouble(inputA);
+		double b = Double.parseDouble(inputB);
+		double c = Double.parseDouble(inputC);
+
+		// colocar uma asserção devido a palavra 'deve'
+		assertThat(Bhaskara.calcularRaizes(a, b, c)).contains(-1.0, 3.0);
+	}
+}
