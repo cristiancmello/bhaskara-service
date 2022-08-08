f52047a8b46741e554a0c6f73be1d4ce51957b3a
ezequiel.moura <ezequiel.moura@inmetrics.com.br>
2022-08-08 14:24:20 -0300

    latest

--- src/test/java/com/bhaskara/service/BhaskaraCfTests.java
+++ src/test/java/com/bhaskara/service/BhaskaraCfTests.java
@@ -8,44 +8,48 @@ import com.amazonaws.services.cloudformation.model.DeleteStackRequest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class BhaskaraCfTests {
-    @AfterEach
-    void cleanup() {
-        var cfClient = AmazonCloudFormationClientBuilder
-                .standard()
-                .withRegion(Regions.US_EAST_1)
-                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
-                .build();
-
-        var deleteStackRequest = new DeleteStackRequest();
-        deleteStackRequest.setStackName("bhaskara-stack");
-        cfClient.deleteStack(deleteStackRequest);
-    }
+//       @AfterEach
+//    void cleanup() {
+//        var cfClient = AmazonCloudFormationClientBuilder
+//                .standard()
+//                .withRegion(Regions.US_EAST_1)
+//                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
+//                .build();
+//
+//        var deleteStackRequest = new DeleteStackRequest();
+//        deleteStackRequest.setStackName("bhaskaraStackTeste");
+//        cfClient.deleteStack(deleteStackRequest);
+//    }
 
     @Test
-    void givenCfFileAndEcrContainerRegistry_whenDeploy_thenEcsFargateClusterDeployment() {
+    void givenCfFileAndEcrContainerRegistry_whenDeploy_thenEcsFargateClusterDeployment() throws IOException {
         // TODO: Criar arquivo Cloudformation que define resources: ECS Cluster, TaskDefinition, Service
         // TaskDefinition: https://docs.aws.amazon.com/pt_br/AWSCloudFormation/latest/UserGuide/aws-resource-ecs-taskdefinition.html
         // Service: https://docs.aws.amazon.com/pt_br/AWSCloudFormation/latest/UserGuide/aws-resource-ecs-service.html
         // TODO: Importar SDK ECS para montar testes de integração para saber se resources foram devidamente criados
         // TODO: Refatorar para extrair comando de deploy de infra AWS
+
         var cfClient = AmazonCloudFormationClientBuilder
                 .standard()
                 .withRegion(Regions.US_EAST_1)
                 .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
                 .build();
 
+        Path filePath = Path.of("BhaskaraCloudformationStack.yml");
+
         var createStackRequest = new CreateStackRequest();
-        createStackRequest.setStackName("bhaskara-stack");
-        createStackRequest.setTemplateBody("AWSTemplateFormatVersion: 2010-09-09\n" +
-                "\n" +
-                "Resources:\n" +
-                "  ECSCluster:\n" +
-                "    Type: 'AWS::ECS::Cluster'\n" +
-                "    Properties:\n" +
-                "      ClusterName: BhaskaraCluster"); // TODO: carregar string do arquivo BhaskaraCloudformationStack.yml
+        createStackRequest.setStackName("bhaskaraStackTesteLast");
+        createStackRequest.setCapabilities(List.of("CAPABILITY_IAM"));
+        createStackRequest.setTemplateBody(Files.readString(filePath)); // TODO: carregar string do arquivo BhaskaraCloudformationStack.yml
 
         cfClient.createStack(createStackRequest);
     }
